61fb7e9e09f0cda60336c4aca14baf99c48af2f9
mgznyuan <51447326+mgznyuan@users.noreply.github.com>
2020-06-18 15:43:56 -0400

    Update first_file.py
    Third edits

--- first_file.py
+++ first_file.py
@@ -1,3 +1,15 @@
 print("hello world")
 print ()
 # THIS IS A GIT TESTING FILE
+Class Pet():
+  def __init__(self,name):
+    self.name = name
+    self.age= age
+    self.sound=sound
+  def __str__(self):
+    return "I am " + str(self.name) + ". I am " + str(self.age) + " years old." + str(self.sound) + "!"
+name= input("Name: \n")
+age= input ("Age: \n")
+sound =input("Sound: \n")
+print(a.__str__())
+  
